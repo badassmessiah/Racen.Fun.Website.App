38aa2572b850d89bff7f34489398121d9119a206
badassmessiah <lashaandguladze69@gmail.com>
2024-11-19 22:39:38 +0400

    Add country selection and enhance contact form validation

--- Racen.Fun.Website/Components/Pages/Contact.razor.cs
+++ Racen.Fun.Website/Components/Pages/Contact.razor.cs
@@ -3,6 +3,10 @@ using System.IO;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Configuration;
 using Microsoft.JSInterop;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Racen.Fun.Website.Components.Pages
 {
@@ -11,26 +15,27 @@ namespace Racen.Fun.Website.Components.Pages
         private ContactModel contactModel = new ContactModel();
         private string csvFilePath;
         private string successMessage;
+        private List<Country> countries = new List<Country>();
         [Inject] private IJSRuntime JS { get; set; }
 
         protected override void OnInitialized()
         {
-
             var appSettingsPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
-
             csvFilePath = Path.Combine(Path.GetDirectoryName(appSettingsPath), "emails.csv");
+
+            /// Load countries from Configuration
+            var countriesJsonPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "countries", "Countries.json");
+            var countriesJson = File.ReadAllText(countriesJsonPath);
+            countries = JsonSerializer.Deserialize<List<Country>>(countriesJson);
         }
 
         private async Task HandleValidSubmit()
         {
-
             SaveEmailToCsv(contactModel.Email);
             successMessage = "Thank you for registering!";
             Console.WriteLine("Form submitted successfully!");
 
             await Task.Delay(2000);
-
-      
             await JS.InvokeVoidAsync("scrollToElement", "main");
         }
 
@@ -51,6 +56,12 @@ namespace Racen.Fun.Website.Components.Pages
 
         public class ContactModel
         {
+            [Required(ErrorMessage = "Name is required.")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Country is required.")]
+            public string Country { get; set; }
+
             [Required(ErrorMessage = "Email is required.")]
             [EmailAddress(ErrorMessage = "Invalid email address.")]
             public string Email { get; set; }
---
+++ Racen.Fun.Website/Country.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+
+namespace Racen.Fun.Website
+{
+    public class Country
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        
+        [JsonPropertyName("code")]
+        public string Code { get; set; }
+    }
+}
