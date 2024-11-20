947b7d30b410e43d9c56b769df2c4b88dad4c3a1
badassmessiah <lashaandguladze69@gmail.com>
2024-11-20 18:05:45 +0400

    Refactor Navbar component for improved navigation and add Paper page with game details

--- Racen.Fun.Website/Components/Layout/Navbar.razor.cs
+++ Racen.Fun.Website/Components/Layout/Navbar.razor.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
 namespace Racen.Fun.Website.Components.Layout
@@ -7,15 +6,39 @@ namespace Racen.Fun.Website.Components.Layout
 	public partial class Navbar
 	{
 		private bool isNavbarCollapsed = false;
-		
 
-		public void ToggleNavbar()
+		[Inject]
+		private NavigationManager NavigationManager { get; set; }
+
+		[Inject]
+		private IJSRuntime JSRuntime { get; set; }
+
+		private void ToggleNavbar()
 		{
 			isNavbarCollapsed = !isNavbarCollapsed;
 		}
 
-        
+		private void NavigateToMain() => NavigateToSection("main");
+		private void NavigateToGame() => NavigateToSection("game");
+		private void NavigateToContact() => NavigateToSection("contact");
 
-        
-    }
+		private void NavigateToSection(string sectionId)
+		{
+			var uri = NavigationManager.ToAbsoluteUri(NavigationManager.Uri);
+			if (uri.AbsolutePath != "/")
+			{
+				NavigationManager.NavigateTo($"/#{sectionId}", forceLoad: true);
+			}
+			else
+			{
+				ScrollToSection(sectionId);
+			}
+		}
+
+		private void ScrollToSection(string sectionId)
+		{
+			var script = $"document.getElementById('{sectionId}').scrollIntoView({{behavior: 'smooth'}});";
+			JSRuntime.InvokeVoidAsync("eval", script);
+		}
+	}
 }
